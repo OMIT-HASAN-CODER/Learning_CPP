b2c3e8e3f7b1ccb0b522e6bf6b3a22a2cc82ea51
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-04-28 16:47:38 +0600

    varconst 3rd commit

--- 01_varconst/main.cpp
+++ 01_varconst/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main() {
     int score;
     score = 110;
-    
+
+    int balance = 500;
+    balance = 1000;
+
     cout << "Welcome to VarConst!" << endl;
 
     return 0;

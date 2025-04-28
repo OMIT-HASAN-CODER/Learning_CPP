de539c9f2f959fa0e4efa2eef424d1f9bc62a3e3
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-04-28 16:36:37 +0600

    hello world 2nd commit

--- 00_hello/main.cpp
+++ 00_hello/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
+
+using std::cout;
+using std::endl;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    // cout << "Hello, World!" << endl;
+
+    cout << "Hello, World!" << std::endl;
 
     return 0;
 }

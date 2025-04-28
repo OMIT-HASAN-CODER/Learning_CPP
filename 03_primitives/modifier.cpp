7f003176dca67e89d5a3904d7607b026e8bf1fcd
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-04-29 01:59:22 +0600

    datatypes 2nd commit

---
+++ 03_primitives/modifier.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    unsigned smallTeaPack = 1200;
+    long largeTeaStorage = 1000000000;
+    // long long largeTeaStorage = 1000000000000000000;
+
+    short teaSample = 25;
+    
+    cout << largeTeaStorage << endl;
+
+    return 0;
+}

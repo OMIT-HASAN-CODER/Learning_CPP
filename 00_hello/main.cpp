a3e418f851f88890a03ccba8a654aa3fd5cfa0d1
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-04-28 16:37:57 +0600

    hello world 3rd commit

--- 00_hello/main.cpp
+++ 00_hello/main.cpp
@@ -2,13 +2,15 @@
 
 // using namespace std;
 
-using std::cout;
-using std::endl;
+// using std::cout;
+// using std::endl;
 
 int main() {
     // cout << "Hello, World!" << endl;
 
-    cout << "Hello, World!" << std::endl;
+    // cout << "Hello, World!" << std::endl;
+
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }

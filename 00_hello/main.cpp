0feb61fc353929a10e1fbe2b93e62a908ca5341b
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-04-28 16:39:53 +0600

    hello world 4th commit

--- 00_hello/main.cpp
+++ 00_hello/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-// using namespace std;
+using namespace std;
 
+// different ways to include the iostream header and use the std namespace
 // using std::cout;
 // using std::endl;
 
 int main() {
-    // cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << endl;
 
     // cout << "Hello, World!" << std::endl;
 
-    std::cout << "Hello, World!" << std::endl;
+    // different ways to include the iostream header and use the std namespace
+    // std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }

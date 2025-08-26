a97ace251c73685e1d1b9f9f2820f237ca796d31
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-26 22:04:22 +0600

    data first commit

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int teaLeaves = 50;
+    float waterTemperature = 85.5;
+    double priceOfTea = 299.99;
+    char teaGrade = 'A';
+    bool isTeaReady = false;
+
+    cout << teaLeaves << endl;
+    cout << waterTemperature << endl;
+    cout << priceOfTea << endl;
+    cout << teaGrade << endl;
+    cout << isTeaReady << endl;
+
+    return 0;
+}

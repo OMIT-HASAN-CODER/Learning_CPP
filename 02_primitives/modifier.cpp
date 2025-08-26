e06e148d4df8816a907b1cdeadb28601c926b31f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-26 22:19:43 +0600

    modifier first commit

---
+++ 02_primitives/modifier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned smallTeaPack = 1200;
+
+    long long largeTeaStorage = 1000000000000000000;
+
+    short teaSample = 25;
+
+    cout << teaSample << endl;
+    return 0;
+}

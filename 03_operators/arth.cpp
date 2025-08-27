c4786c64b6396da449f9a03d09ffee74c68b9b69
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-27 22:08:03 +0600

    arth first commit

---
+++ 03_operators/arth.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int cups;
+    double pricePerCup, totalPrice, discountedPrice;
+
+    cout << "Enter the number of tea cups: " << endl;
+    cin >> cups;
+    cout << "Enter the price per cup: " << endl;
+    cin >> pricePerCup;
+
+    totalPrice = cups * pricePerCup;
+
+    // apply 5% discount if total price is above 100
+    if (totalPrice > 100) {
+        discountedPrice = totalPrice - (totalPrice * 0.05);
+        cout << "Discounted Price is: " << discountedPrice << endl;
+    } else {
+        cout << "Total Price is: " << totalPrice << endl;
+    }
+}

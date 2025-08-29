97df511ffda8859377ad705473a4a3c397a70149
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-29 23:52:38 +0600

    break first commit

---
+++ 05_loops/break.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string response;
+
+    while (true) {
+        cout << "Do you want more tea (type 'stop' to exit)? " << endl;
+        getline(cin, response);
+
+        if (response == "stop") {
+            //exit the loop
+            break;
+        }
+
+        cout << "Here is your another cup of tea.\n" << endl;
+    }
+
+    cout << "No more tea will be served to you." << endl;
+
+    return 0;
+}

5f6922a209808bd10a6af6dc1eadbd4ca14c28df
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-31 00:24:56 +0600

    my function third commit

--- 06_function/myFunction.cpp
+++ 06_function/myFunction.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,15 +10,25 @@ int checkTemperature(int temperature) {
 //declaration of function
 void serveChai(int cups);
 
+void makeChai() {
+    cout << "Boiling water, adding tea leaves, straining..." << endl;
+}
+
+void serveChai(string teaType = "Masala Tea") {
+    int cups = 8;
+    cout << "Serving " << teaType << endl;
+}
+
 int main() {
     int temp = checkTemperature(50);
     cout << temp << endl;
-
+    makeChai(); //calling a function
     serveChai(3);
+    serveChai();
     return 0;
 }
 
 //definition of function
 void serveChai(int cups) {
-    cout << "Serving " << cups << " of chai";
+    cout << "Serving " << cups << " of chai" << endl;
 }

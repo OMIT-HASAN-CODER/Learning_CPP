de038ad85a8fb498384b9ee623369a8ba390b077
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-31 00:13:35 +0600

    my function second commit

--- 06_function/myFunction.cpp
+++ 06_function/myFunction.cpp
@@ -6,8 +6,18 @@ int checkTemperature(int temperature) {
     return temperature;
 }
 
+//declaration of function
+void serveChai(int cups);
+
 int main() {
     int temp = checkTemperature(50);
     cout << temp << endl;
+
+    serveChai(3);
     return 0;
+}
+
+//definition of function
+void serveChai(int cups) {
+    cout << "Serving " << cups << " of chai";
 }

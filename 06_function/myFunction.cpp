41a0972c5b177fba2a6ef369ed67d19b68c4b5e6
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-08-31 00:05:14 +0600

    my function first commit

---
+++ 06_function/myFunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int checkTemperature(int temperature) {
+    return temperature;
+}
+
+int main() {
+    int temp = checkTemperature(50);
+    cout << temp << endl;
+    return 0;
+}

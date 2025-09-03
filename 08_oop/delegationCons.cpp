ec2ecafd4ab197a8f581e069a2ced976fe79ba70
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-09-04 01:23:38 +0600

    delegation constructor first commit

---
+++ 08_oop/delegationCons.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Chai {
+public:
+    string teaName;
+    int servings;
+    vector<string> ingredients;
+
+    //delegating constructor
+    Chai(string name) : Chai(name, 1, {"Water", "Tea Leaves"}) {}
+
+    //main constructor
+    Chai(string name, int serve, vector<string> ingr) {
+        teaName = name;
+        servings = serve;
+        ingredients = ingr;
+
+        cout << "Main Constructor called!" << endl;
+    }
+
+    void displayChaiDetails() {
+        cout << "Tea Name: " << teaName << endl;
+        cout << "Tea Servings: " << servings << endl;
+        cout << "Ingredients: ";
+        for (string ingredient : ingredients) {
+            cout << ingredient << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Chai quickChai("Quick Chai");
+    quickChai.displayChaiDetails();
+
+    return 0;
+}
